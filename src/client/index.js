831b46f0f29f0e4361c591999670dbac46b19e40
Rony Soliman <83412181+RonySoliman@users.noreply.github.com>
2024-08-22 10:20:33 +0300

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import formHandler from './js/formHandler';
+import buttonHandler from './js/formHandler';
+
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+
+export { 
+    formHandler,
+    buttonHandler 
+};
---
+++ src/client/sub.test.js
@@ -0,0 +1,5 @@
+const sub = require('./sub');
+
+test('sub 10 - 5 to equal 5', () => {
+  expect(sub(10, 5)).toBe(5);
+});
